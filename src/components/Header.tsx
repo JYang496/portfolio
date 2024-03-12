bc9c9e21dad621759764be075b4808e1151b986b
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-03-11 23:45:37 -0400

    content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,11 +9,7 @@ export const Header = () => {
               <div className="profile">
                       <h1 className="text-light"><a href="/">Jinhua Yang</a></h1>
                       <div className="social-links mt-3 text-center">
-                          <a href="/" className="twitter"><i className="bx bxl-twitter"></i></a>
-                          <a href="/" className="facebook"><i className="bx bxl-facebook"></i></a>
-                          <a href="/" className="instagram"><i className="bx bxl-instagram"></i></a>
-                          <a href="/" className="google-plus"><i className="bx bxl-skype"></i></a>
-                          <a href="/" className="linkedin"><i className="bx bxl-linkedin"></i></a>
+                          <a href="https://www.linkedin.com/in/jinhua-yang-494186198/" className="linkedin"><i className="bx bxl-linkedin"></i></a>
                       </div>
               </div>
 
